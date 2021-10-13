2ae017a367c1b0ae3b1aaddcdc1f4f3b6ac44ece
Josue Viana <josuep.viana@gmail.com>
2021-10-13 17:15:40 -0300

    requirements atualizado

--- exemplos/lendo_yml.py
+++ exemplos/lendo_yml.py
@@ -1,7 +1,7 @@
 import yaml
 
 
-with open('./data/static/cid10_queimaduras.yml', 'r', encoding="utf-8") as stream:
+with open('./data/static/config.yml', 'r', encoding="utf-8") as stream:
     try:
         print(yaml.safe_load(stream))
     except yaml.YAMLError as exc:
