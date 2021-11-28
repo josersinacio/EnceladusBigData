064dc7cf370d8fe9f39bd653ffaaa944ed63a2c7
José Rodrigo <jose.rs.inacio@gmail.com>
2021-11-28 15:10:38 -0300

    [Develop] Initial Commit

--- exemplos/lendo_yml.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-
-with open('./data/static/cid10_queimaduras.yml', 'r', encoding="utf-8") as stream:
-    try:
-        print(yaml.safe_load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
